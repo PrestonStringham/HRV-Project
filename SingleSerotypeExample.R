a9e9171b2d15740a4680f65a0828871d7c64ecb9
Unknown <stringhampreston@gmail.com>
2019-01-08 23:17:53 -0700

    Fixing Issues
    IDK

--- SingleSerotypeExample.R
+++ SingleSerotypeExample.R
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 # ------ The following script is an example project for the Diversity of viruses project -------#
-=======
-# ------ The following script analyzes the infectivity of viruses which have one serotype -------#
->>>>>>> master
 
 b <- 0
 g <- 0
