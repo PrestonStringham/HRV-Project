6cab7806c0e1224dde6003e7eaa245837f346625
Unknown <stringhampreston@gmail.com>
2019-01-17 01:05:48 -0700

    Begin Model (Currently Not Working)

--- DiversityModel.R
+++ DiversityModel.R
@@ -1,5 +1,17 @@
-b <- 0
-g <- 0
-N <- 0
-m <- 0
+N <- 0;
+m <- 10;
+g <- c(1:m);
+bn <- c(1:m);
+
+A <- matrix(0, nrow = m, ncol = m)
+
+x <- as.list(rnorm(10));
+names(x) <- paste("a", 1:length(x), sep = "")
+list2env(x , envir = .GlobalEnv)
+
+for(index in 1:m){
+  A[index, index] <- 3
+}
+
+d <- A*bn
 
