7f89722dc39a7244085b3b52321d79bf929ea67e
Preston <stringhampreston@gmail.com>
2019-02-18 22:20:09 -0700

    Finished Gillespie (Missing ggplot2 figures)

--- DiversityModelGillespie.R
+++ DiversityModelGillespie.R
@@ -1,18 +1,17 @@
+#Preston Stringham - January 2019#
+# Rhinovirus Diversity Experiment. Required packages: googlesheets, ggplot2#
 library(googlesheets)
 
 #ACCESS SPREADSHEET#
-
 master_sheet <- gs_title("HRV-Experiment")
 
 
 #SET CONSTANTS USING SPREADSHEET #
-
 N <- as.numeric(as.list(gs_simplify_cellfeed(gs_read_cellfeed(master_sheet, ws = 1, range=cell_cols(4)))))
 mu <- as.numeric(as.list(gs_simplify_cellfeed(gs_read_cellfeed(master_sheet, ws = 1, range=cell_cols(5)))))
 
 
 #SET INFECTION AND RECOVERY RATES (bn and gn, respectively)#
-
 b<-gs_read_cellfeed(master_sheet, ws = 1, range=cell_cols(1))
 bn <- as.numeric(as.list(gs_simplify_cellfeed(b)))
 
@@ -21,7 +20,6 @@ gn <- as.numeric(as.list(gs_simplify_cellfeed(g)))
 
 
 #SET INITIAL INFECTED POPULATION DATA#
-
 I<-gs_read_cellfeed(master_sheet, ws = 1, range=cell_cols(3))
 In <- as.numeric(as.list(gs_simplify_cellfeed(I)))
 
@@ -29,64 +27,88 @@ if(length(bn) != length(gn) && length(bn) != length(In)){
   stop("Not enough infection rates, recovery rates, or initial infected population... Please fix...")
 }else{
   
+  #m SEROTYPES
   m <- length(gn)
-  t <- 0
   
+  #INITIAL TIME
+  time <- 0
+  
+  #SIMULATION DURATION
+  duration <- 0.1
+  
+  #INITIAL SUSCEPTIBLE POPULATION
   S <- N-sum(In)
+  S_vals <- list()
+  for(i in c(1:m)){
+    val <- list(S)
+    S_vals[i] <- list(val)
+  }
   
-  Time <- 10
-  
-  S_vals <- list(S)
-  
+  #GENERATE LIST OF LISTS FOR INITIAL POPULATION OF EACH SEROTYPE
   I_vals <-list()
   
   for(i in c(1:m)){
-    test <- list(In[i])
-    I_vals[i] <- list(test)
+    val <- list(In[i])
+    I_vals[i] <- list(val)
   }
   
-  t_vals <- list()
+  #GENERTATE LIST OF LISTS FOR TIME STEPS OF EACH SEROTYPE IN SIMULATION
+  time_vals <- list()
   
   for(i in c(1:m)){
-     val <- list(t)
-     t_vals[i] <- list(val)
+     val <- list(time)
+     time_vals[i] <- list(val)
   }
   
+  count_1 <- 0
+  count_2 <- 0
+  count_3 <- 0
+  
+  #RUN SIMULATION m TIMES
   for(i in c(1:m)){
     
-    t <- 0
+    time <- 0
     
-    while (t < Time){
+    while (time < duration){
       
-      w1 <- (mu/(m-1))*(bn[i]*In[i]*S)/N;
-      w2 <- gn[i]*In[i];
-      W <- w1 + w2
+      #SUM OF THE RATE OF THE EVENTS
+      R1 <- (mu/(m-1))*(bn[i]*In[i]*S)/N;
+      R2 <- gn[i]*In[i];
+      R <- R1 + R2
       
-      dt <- -log(runif(1))/W
+      # GENERATE TIME STEP dt
+      dt <- -log(runif(1))/R
       
-      t <- t + dt
-        
-      if(runif(1) < w1/W){
+      #ADD dt TO time
+      time <- time + dt
+      
+      #CHOOSE OUTCOME BASED ON RANDOM NUMBER AND SUM OF RATE OF EVENTS
+      rand <- runif(1)
+      if(rand < R1/R){
         S <- S-1
         In[i] <- In[i]+1
+        count_1 <- count_1 + 1
       }
-      
-      else{
+      else if(rand < R2/R){
         In[i] <- In[i]-1
         S <- S+1
+        count_2 <- count_2 + 1
+      }
+      else{
+        S <- S
+        In[i] <- In[i]
+        count_3 <- count_3 + 1
       }
       
-      #S_vals <- c(S_vals, S)
-      I_vals[[i]] <- c(I_vals[[i]], In[i])
-      t_vals[[i]] <- c(t_vals[[i]], t)
+      #APPEND VALUES TO PLOT
+      S_vals[[i]] <- c(S_vals[[i]], S)
+      I_vals[[i]] <- c(I_vals[[i]], In[[i]])
+      time_vals[[i]] <- c(time_vals[[i]], time)
     }
   }
 }
-#plot(t_vals[[1]], S_vals)
-#for(i in c(1:m)){
-par(mfrow=c(2,2))
-  plot(t_vals[[1]], I_vals[[1]])
-  plot(t_vals[[2]], I_vals[[2]])
-  plot(t_vals[[3]], I_vals[[3]])
-  plot(t_vals[[4]], I_vals[[4]])
-#}
+
+#PLOT DATA
+par(mfrow=c(1,m/2))
+plot(time_vals[[1]], S_vals[[1]], type="l")
+plot(time_vals[[1]], I_vals[[1]], type="l")
